8a1cbe80dcd9eb3f25000d4b1005ba5a95df6d4b
fengchangxin <1534891720@qq.com>
2020-10-19 22:54:12 +0800

    初始化

---
+++ client2/src/main/java/com/fs/client2/Client2Application.java
@@ -0,0 +1,16 @@
+package com.fs.client2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: fengchangxin
+ * @date: 2020/10/19:22:41
+ * @description:
+ **/
+@SpringBootApplication
+public class Client2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Client2Application.class, args);
+    }
+}
